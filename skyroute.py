2cd9f56b0c584941758f6e60f18e127cf0a4527d
Christopher <chris@theantichris.com>
2020-01-10 12:30:01 -0500

    created a string to hold letter, landmark pairs

--- skyroute.py
+++ skyroute.py
@@ -4,3 +4,8 @@ from vc_landmarks import vc_landmarks
 from landmark_choices import landmark_choices
 
 # Build your program below:
+landmark_string = ""
+for letter, landmark in landmark_choices.items():
+    landmark_string += "{} - {}\n".format(letter, landmark)
+
+print(landmark_string)
